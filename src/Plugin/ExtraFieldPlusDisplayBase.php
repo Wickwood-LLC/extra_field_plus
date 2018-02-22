1c7de990dd992bb53f61db9feb0a74a644ddede7
justin <andrii.tspkh@gmail.com>
2018-02-22 20:33:04 +0200

    Fixed problem with entity_view_display before a display object is explicitly configured and saved.

--- src/Plugin/ExtraFieldPlusDisplayBase.php
+++ src/Plugin/ExtraFieldPlusDisplayBase.php
@@ -2,12 +2,15 @@
 
 namespace Drupal\extra_field_plus\Plugin;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\extra_field\Plugin\ExtraFieldDisplayBase;
 
 /**
  * Base class for Extra Field Plus Display plugins.
  */
 abstract class ExtraFieldPlusDisplayBase extends ExtraFieldDisplayBase implements ExtraFieldPlusDisplayInterface {
+
+  use StringTranslationTrait;
 
   /**
    * {@inheritdoc}
